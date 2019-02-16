fa9294e019f806dbef6907ad843993b29e874c98
Germain Masse <gmasse@users.noreply.github.com>
2019-02-16 18:58:52 +0100

    Update openrc.sh
    Fallback to command line password prompt
    Do not override $OS_REGION_NAME

--- OpenStack/openrc.sh
+++ OpenStack/openrc.sh
@@ -8,7 +8,6 @@
 export OS_AUTH_URL=https://auth.cloud.ovh.net/v2.0/
 export OS_IDENTITY_API_VERSION=2
 
-
 # With the addition of Keystone we have standardized on the term **tenant**
 # as the entity that owns the resources.
 export OS_TENANT_ID=1234
@@ -19,14 +18,33 @@ export OS_TENANT_NAME=5678
 export OS_USERNAME=johndoe
 
 # With Keystone you pass the keystone password.
-#echo "Please enter your OpenStack Password: "
-#read -sr OS_PASSWORD_INPUT
-#export OS_PASSWORD=$OS_PASSWORD_INPUT
-export OS_PASSWORD=$(security find-generic-password -a ${OS_USERNAME} -s "OVH Public Cloud" -w)
-
+export OS_PASSWORD=
+# MacOS environment?
+if [ "$(uname)" = "Darwin" ];
+then
+    # Test if `security` binary exists
+    if [ -x "$(command -v 'security')" ];
+    then
+        # Look for the password in the keychain
+        if security find-generic-password -a ${OS_USERNAME} -s "OVH Public Cloud" >/dev/null 2>&1;
+        then
+            export OS_PASSWORD=$(security find-generic-password -a ${OS_USERNAME} -s "OVH Public Cloud" -w)
+        fi
+    fi
+fi
+# Fallback to command line password prompt
+if [ -z $OS_PASSWORD ];
+then
+    echo "Please enter OpenStack Password for User=$OS_USERNAME: "
+    read -sr OS_PASSWORD_INPUT
+    export OS_PASSWORD=$OS_PASSWORD_INPUT
+fi
 
 # If your configuration has multiple regions, we set that information here.
+if [ -z "$OS_REGION_NAME" ];
+then
 # OS_REGION_NAME is optional and only valid in certain environments.
-export OS_REGION_NAME="GRA5"
+    export OS_REGION_NAME="GRA5"
+fi
 # Don't leave a blank variable, unset it if it was empty
 if [ -z "$OS_REGION_NAME" ]; then unset OS_REGION_NAME; fi
